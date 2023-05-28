b3ce44f8665d0552854b01881e801c2fc5ec4e84
Yogesh More <moreyog@gmail.com>
2023-05-28 22:09:23 +0530

    initial commit

---
+++ src/main/java/com/eaf/discoveryservice/DiscvoeryApplication.java
@@ -0,0 +1,15 @@
+package com.eaf.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscvoeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscvoeryApplication.class, args);
+	}
+
+}
--- src/test/java/com/eaf/discoveryservice/EafTestServiceApplicationTests.java
+++ src/test/java/com/eaf/discoveryservice/EafTestServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eaf.eaftestservice;
+package com.eaf.discoveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
